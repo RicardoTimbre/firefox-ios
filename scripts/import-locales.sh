9ecb3005b433fd4ab44f3c938a7e6eaceca77f50
Stefan Arentz <stefan@arentz.ca>
2015-04-10 16:36:08 -0400

    Fixes 1136220 - Import localizations from the shared SVN repository

---
+++ scripts/import-locales.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+scripts/xliff-cleanup.py ../firefox-ios-l10n/*/*.xliff
+
+rm -rf imported-locales
+mkdir imported-locales
+
+scripts/xliff-export.py ../firefox-ios-l10n imported-locales
+
