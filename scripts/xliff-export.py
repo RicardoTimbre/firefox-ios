5efb0e2f2552f74ba05f8068d33aa5d30d7ffe8a
Stefan Arentz <stefan@arentz.ca>
2015-04-10 20:11:39 -0400

    Fixes 1136220 - Import localizations from the shared SVN repository

--- scripts/xliff-export.py
+++ scripts/xliff-export.py
@@ -35,7 +35,7 @@ FILES = [
     "Extensions/ShareTo/Localizable.strings"
 ]
 
-def export_xliff_file(file_node, export_path):
+def export_xliff_file(file_node, export_path, target_language):
     directory = os.path.dirname(export_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -44,6 +44,10 @@ def export_xliff_file(file_node, export_path):
             sources = trans_unit_node.xpath("x:source", namespaces=NS)
             targets = trans_unit_node.xpath("x:target", namespaces=NS)
 
+            # Special hack for en-US.
+            if target_language == "en":
+                targets = sources
+
             if len(sources) == 1 and len(targets) == 1:
                 notes = trans_unit_node.xpath("x:note", namespaces=NS)
                 if len(notes) == 1:
@@ -51,6 +55,11 @@ def export_xliff_file(file_node, export_path):
                     fp.write(line.encode("utf8"))
                 line = u"\"%s\" = \"%s\";\n\n" % (sources[0].text, targets[0].text)
                 fp.write(line.encode("utf8"))
+
+def original_path(root, target, original):
+    parts = original.split(os.sep)
+    lproj = "%s.lproj" % target_language
+    return os.path.join(root, parts[-2], lproj, parts[-1])
 
 if __name__ == "__main__":
 
@@ -65,15 +74,37 @@ if __name__ == "__main__":
         sys.exit(1)
 
     for xliff_path in glob.glob(import_root + "/*/firefox-ios.xliff"):
-        print "Processing ", xliff_path
+        print "Exporting", xliff_path
         with open(xliff_path) as fp:
             tree = etree.parse(fp)
             root = tree.getroot()
-            for file_node in root.xpath("//x:file", namespaces=NS):
+
+            # Make sure there are <file> nodes in this xliff file.
+            file_nodes = root.xpath("//x:file", namespaces=NS)
+            if len(file_nodes) == 0:
+                print "  ERROR: No translated files. Skipping."
+                continue
+
+            # Take the target language from the first <file>. Not sure if that
+            # is a bug in the XLIFF, but in some files only the first node has
+            # the target-language set.
+            source_language = file_nodes[0].get('source-language')
+            target_language = file_nodes[0].get('target-language')
+            if not target_language:
+                # Special hack for en-US. It does not have target-language set but
+                # we still want to write out string files for it. Because if we do
+                # not, english systems will default to some random language.
+                if "en-US" in xliff_path:
+                    target_language = 'en'
+                else:
+                    print "  ERROR: Missing target-language. Skipping."
+                    continue
+
+            # Export each <file> node as a separate strings file under the
+            # export root.
+            for file_node in file_nodes:
                 original = file_node.get('original')
-                target_language = file_node.get('target-language')
-                if original in FILES and target_language:
-                    path_components = original.split(os.sep)
-                    export_path = os.path.join(export_root, path_components[-2], "%s.lproj" % target_language, path_components[-1])
-                    print "  Generating ", export_path
-                    export_xliff_file(file_node, export_path)
+                if original in FILES:
+                    export_path = original_path(export_root, target_language, original)
+                    print "  Writing", export_path
+                    export_xliff_file(file_node, export_path, target_language)
